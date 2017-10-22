7288bdf534f9f1d2b9121d90395508af1a6bd17c
SJJ <978622005@qq.com>
2017-10-22 18:58:16 +0800

    BooksPresenter.kt

--- app/src/main/java/sjj/fiction/books/BookrackContract.kt
+++ app/src/main/java/sjj/fiction/books/BookrackContract.kt
@@ -1,16 +1,26 @@
 package sjj.fiction.books
 
+import android.content.Context
 import sjj.fiction.BasePresenter
 import sjj.fiction.BaseView
+import sjj.fiction.model.Book
+import sjj.fiction.model.BookGroup
 
 /**
  * Created by SJJ on 2017/10/7.
  */
 interface BookrackContract {
     interface Presenter : BasePresenter {
-
+        fun onSelectBook(book: BookGroup, context: Context)
+        fun deleteBook(book: BookGroup)
     }
-    interface View : BaseView<Presenter> {
 
+    interface View : BaseView<Presenter> {
+        fun setBookList(book: List<BookGroup>)
+        fun setBookListLoadingHint(active: Boolean)
+        fun setBookListLoadingError(e: Throwable)
+        fun setDeleteBookError(e: Throwable)
+        fun setBookDetailsLoadingHint(active: Boolean)
+        fun setBookDetailsLoadingError(e: Throwable)
     }
 }
--- app/src/main/java/sjj/fiction/books/BookrackFragment.kt
+++ app/src/main/java/sjj/fiction/books/BookrackFragment.kt
@@ -1,5 +1,6 @@
 package sjj.fiction.books
 
+import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -12,6 +13,7 @@ import android.widget.TextView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.fragment_books.*
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.find
 import org.jetbrains.anko.support.v4.alert
 import org.jetbrains.anko.support.v4.indeterminateProgressDialog
@@ -20,6 +22,7 @@ import sjj.alog.Log
 import sjj.fiction.BaseFragment
 import sjj.fiction.R
 import sjj.fiction.details.DetailsActivity
+import sjj.fiction.model.Book
 import sjj.fiction.model.BookGroup
 import sjj.fiction.util.domain
 import sjj.fiction.util.fictionDataRepository
@@ -27,9 +30,12 @@ import sjj.fiction.util.fictionDataRepository
 /**
  * Created by SJJ on 2017/10/7.
  */
-class BookrackFragment : BaseFragment() {
-    val data: MutableList<BookGroup> = mutableListOf()
-    private var compDisposable: CompositeDisposable = CompositeDisposable()
+class BookrackFragment : BaseFragment(), BookrackContract.View {
+    private val data: MutableList<BookGroup> = mutableListOf()
+    private lateinit var presenter: BookrackContract.Presenter
+    private val adapter by lazy { Adapter() }
+    private var loadingHint: ProgressDialog? = null
+    private var loadingDetailsHint: ProgressDialog? = null
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_books, container, false)
     }
@@ -37,69 +43,85 @@ class BookrackFragment : BaseFragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bookList.layoutManager = LinearLayoutManager(context)
-        bookList.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-                return object : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_text_text, parent, false)) {}
-            }
-
-            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-                val bookGroup = data[position]
-                holder.itemView.find<TextView>(R.id.text1).text = bookGroup.bookName
-                holder.itemView.find<TextView>(R.id.text2).text = bookGroup.author
-                holder.itemView.setOnClickListener { v ->
-                    startActivity(v.context, bookGroup)
-                }
-                holder.itemView.setOnLongClickListener {
-                    alert {
-                        title = "确认删除？"
-                        message = "确认删除书籍：${bookGroup.bookName}？"
-                        negativeButton("取消", {})
-                        positiveButton("删除") {
-                            fictionDataRepository.deleteBookGroup(bookGroup.bookName, bookGroup.author)
-                                    .observeOn(AndroidSchedulers.mainThread())
-                                    .subscribe({
-                                        toast("删除成功")
-                                    }, {
-                                        toast("删除失败：${it.message}")
-                                    })
-                        }
-                    }.show()
-                    true
-                }
-            }
-
-            override fun getItemCount(): Int = data.size
-            fun startActivity(context: Context, book: BookGroup) {
-                val dialog = indeterminateProgressDialog("请稍候")
-                compDisposable.add(fictionDataRepository.loadBookDetailsAndChapter(book)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe({
-                            dialog.dismiss()
-                            val intent = Intent(context, DetailsActivity::class.java);
-                            intent.putExtra(DetailsActivity.data_book_name, it.bookName)
-                            intent.putExtra(DetailsActivity.data_book_author, it.author)
-                            startActivity(intent)
-                        }, {
-                            dialog.dismiss()
-                            Log.e("error", it)
-                        }))
-            }
-        }
+        bookList.adapter = adapter
+        BooksPresenter(this)
     }
 
     override fun onStart() {
         super.onStart()
-        fictionDataRepository.loadBookGroups()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    data.clear()
-                    data.addAll(it)
-                    bookList.adapter.notifyDataSetChanged()
-                }, {})
+        presenter.start()
     }
 
     override fun onStop() {
         super.onStop()
-        compDisposable.clear()
+        presenter.stop()
+    }
+
+    override fun setPresenter(presenter: BookrackContract.Presenter) {
+        this.presenter = presenter
+    }
+
+    override fun setBookList(book: List<BookGroup>) {
+        adapter.data = book
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun setBookListLoadingHint(active: Boolean) {
+        loadingHint = if (active) {
+            loadingHint ?: indeterminateProgressDialog("正在加载书籍列表请稍候……")
+        } else {
+            loadingHint?.dismiss()
+            null
+        }
+    }
+
+    override fun setBookListLoadingError(e: Throwable) {
+        toast("书籍加载出错：${e.message}")
+    }
+
+    override fun setDeleteBookError(e: Throwable) {
+        toast("书籍删除出错：${e.message}")
+    }
+
+    override fun setBookDetailsLoadingHint(active: Boolean) {
+        loadingDetailsHint = if (active) {
+            loadingDetailsHint ?: indeterminateProgressDialog("正在加载书籍内容请稍候……")
+        } else {
+            loadingDetailsHint?.dismiss()
+            null
+        }
+    }
+
+    override fun setBookDetailsLoadingError(e: Throwable) {
+        toast("书籍内容加载失败：${e.message}")
+    }
+
+    private inner class Adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var data: List<BookGroup>? = null
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return object : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_text_text, parent, false)) {}
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val bookGroup = data!![position]
+            holder.itemView.find<TextView>(R.id.text1).text = bookGroup.bookName
+            holder.itemView.find<TextView>(R.id.text2).text = bookGroup.author
+            holder.itemView.setOnClickListener { v ->
+                presenter.onSelectBook(bookGroup, context)
+            }
+            holder.itemView.setOnLongClickListener {
+                alert {
+                    title = "确认删除？"
+                    message = "确认删除书籍：${bookGroup.bookName}？"
+                    negativeButton("取消", {})
+                    positiveButton("删除") {
+                        presenter.deleteBook(bookGroup)
+                    }
+                }.show()
+                true
+            }
+        }
+
+        override fun getItemCount(): Int = data?.size ?: 0
     }
 }
