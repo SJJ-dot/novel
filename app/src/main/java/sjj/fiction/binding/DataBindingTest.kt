a8b7187e279d0adfd5da4f3cfa079333d19849b9
SJJ <978622005@qq.com>
2018-02-08 17:07:58 +0800

    测试viewmodel 是否会造成泄露

--- app/src/main/java/sjj/fiction/binding/DataBindingTest.kt
+++ app/src/main/java/sjj/fiction/binding/DataBindingTest.kt
@@ -8,6 +8,7 @@ import io.reactivex.schedulers.Schedulers
 import sjj.alog.Log
 import sjj.fiction.BaseActivity
 import sjj.fiction.R
+import sjj.fiction.Session
 import sjj.fiction.databinding.ActivityDataBindingTestBinding
 import sjj.fiction.model.User
 import sjj.fiction.util.log
@@ -17,7 +18,7 @@ import sjj.fiction.util.submit
  * Created by sjj on 2017/12/20.
  */
 class DataBindingTest : BaseActivity() {
-    val user = User()
+    val user = Session.user
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val aaa = DataBindingUtil.setContentView<ActivityDataBindingTestBinding>(this, R.layout.activity_data_binding_test)
