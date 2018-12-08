00f4f2f29e6ee56a2cf4f427be2786ef327cde6c
shen <978622005@qq.com>
2018-12-08 21:07:41 +0800

    json

--- app/src/main/java/sjj/novel/data/repository/NovelSourceRepository.kt
+++ app/src/main/java/sjj/novel/data/repository/NovelSourceRepository.kt
@@ -5,6 +5,7 @@ import io.reactivex.Observable
 import io.reactivex.Scheduler
 import io.reactivex.schedulers.Schedulers
 import sjj.novel.data.source.local.booksDataBase
+import sjj.novel.data.source.remote.DefaultNovelSource
 import sjj.novel.data.source.remote.rule.BookParseRule
 
 
@@ -13,7 +14,7 @@ val novelSourceRepository by lazy { NovelSourceRepository() }
  * 小说来源。后续可能还会存到数据库中
  */
 class NovelSourceRepository {
-
+    private val source by lazy { DefaultNovelSource() }
     fun getAllBookParseRule(): Flowable<List<BookParseRule>> {
         return booksDataBase.novelSourceDao().getAllBookParseRule().subscribeOn(Schedulers.io())
     }
@@ -32,5 +33,12 @@ class NovelSourceRepository {
         }.subscribeOn(Schedulers.io())
     }
 
+    fun getDefaultNovelSourceRule(): Observable<List<BookParseRule>> {
+        return source.getDefaultNovelSourceRule()
+    }
+
+    interface RemoteSource {
+        fun getDefaultNovelSourceRule():Observable<List<BookParseRule>>
+    }
 
 }
---
+++ app/src/main/java/sjj/novel/data/source/remote/DefaultNovelSource.kt
@@ -0,0 +1,23 @@
+package sjj.novel.data.source.remote
+
+import io.reactivex.Observable
+import retrofit2.http.GET
+import sjj.novel.data.repository.NovelSourceRepository
+import sjj.novel.data.source.remote.rule.BookParseRule
+
+class DefaultNovelSource : HttpDataSource(), NovelSourceRepository.RemoteSource {
+
+    override val baseUrl: String
+        get() = "https://raw.githubusercontent.com"
+    private val server by lazy { create<Interface>() }
+
+    override fun getDefaultNovelSourceRule(): Observable<List<BookParseRule>> {
+        return server.getDefaultNovelSourceRule()
+    }
+
+    interface Interface {
+        @GET("https://raw.githubusercontent.com/lTBeL/novel/master/novelRule/NovelSource.json")
+        fun getDefaultNovelSourceRule(): Observable<List<BookParseRule>>
+    }
+
+}
