f76908f7539396d1fd50cbb7eb06a4ed0c3b6cdb
shen <978622005@qq.com>
2018-12-09 23:05:56 +0800

    trim

--- app/src/main/java/sjj/novel/data/source/remote/CommonBookEngine.kt
+++ app/src/main/java/sjj/novel/data/source/remote/CommonBookEngine.kt
@@ -43,9 +43,9 @@ class CommonBookEngine(val rule: BookParseRule) : NovelDataRepository.RemoteSour
                 val books = mutableListOf<Book>()
                 elements.forEach { element ->
 
-                    val bookName = element.select(resultRule.name).text(resultRule.nameRegex)
+                    val bookName = element.select(resultRule.name).text(resultRule.nameRegex).trim()
                     Log.i("search bookAuthor bookName:$bookName")
-                    val bookAuthor = element.select(resultRule.author).text(resultRule.authorRegex)
+                    val bookAuthor = element.select(resultRule.author).text(resultRule.authorRegex).trim()
                     Log.i("search bookUrl BookAuthor:$bookAuthor")
                     val bookUrl = element.absUrl(resultRule.bookUrl, response)
                     Log.i("search new Book bookUrl:$bookUrl")
@@ -70,9 +70,9 @@ class CommonBookEngine(val rule: BookParseRule) : NovelDataRepository.RemoteSour
             Log.i("getBook bookUrl introRule:$introRule")
             val bookUrl = document.absUrl(introRule.bookUrl, response)
             Log.i("getBook bookName bookUrl:$bookUrl")
-            val bookName = document.select(introRule.bookName).text(introRule.bookNameRegex)
+            val bookName = document.select(introRule.bookName).text(introRule.bookNameRegex).trim()
             Log.i("getBook bookAuthor bookName:$bookName")
-            val bookAuthor = document.select(introRule.bookAuthor).text(introRule.bookAuthorRegex)
+            val bookAuthor = document.select(introRule.bookAuthor).text(introRule.bookAuthorRegex).trim()
             Log.i("getBook bookCoverSrc bookAuthor:$bookAuthor")
             val bookCoverSrc = document.select(introRule.bookCoverImgUrl).first().absUrl("src")
             Log.i("getBook bookIntro bookCoverSrc:$bookCoverSrc")
@@ -111,7 +111,7 @@ class CommonBookEngine(val rule: BookParseRule) : NovelDataRepository.RemoteSour
         Log.i("getBook foreach bookChapterList")
         element.select(chapterListRule.bookChapterList).forEachIndexed { index, e ->
             val url = e.select(chapterListRule.bookChapterUrl).first().absUrl("href")
-            val name = e.select(chapterListRule.bookChapterName).text(chapterListRule.bookChapterNameRegex)
+            val name = e.select(chapterListRule.bookChapterName).text(chapterListRule.bookChapterNameRegex).trim()
             chapters.add(Chapter(url, book.url, index, name))
         }
         Log.i("getBook chapters chapters size:${chapters.size}")
