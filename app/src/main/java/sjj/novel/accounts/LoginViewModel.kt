f8a409409d9febc2d4585916331d45c062e0457a
shen <978622005@qq.com>
2018-12-15 22:45:09 +0800

    允许直接填写token

--- app/src/main/java/sjj/novel/accounts/LoginViewModel.kt
+++ app/src/main/java/sjj/novel/accounts/LoginViewModel.kt
@@ -1,24 +1,40 @@
 package sjj.novel.accounts
 
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.text.Html
 import io.reactivex.Observable
+import sjj.novel.AppConfig
 import sjj.novel.R
 import sjj.novel.Session
 import sjj.novel.data.source.remote.githubDataSource
 import java.lang.Exception
 
 class LoginViewModel : ViewModel() {
-    val loginPasswordExplanation =Html.fromHtml(Session.ctx.getString(R.string.login_pass_explanation))
     val userName = ObservableField<String>()
     val password = ObservableField<String>()
+
     fun login(): Observable<String> {
         val userName = userName.get()
         val password = password.get()
-        if (userName.isNullOrEmpty()||password.isNullOrEmpty()) {
-            return Observable.error<String>(Exception("用户名与密码不能为空"))
+
+        if (password.isNullOrEmpty()) {
+            return Observable.error(Exception("密码不能为空"))
         }
-       return githubDataSource.login(userName, password)
+
+        if (userName.isNullOrEmpty()) {
+            //授权令牌（Personal access token） 只填密码 视为token
+            return Observable.fromCallable {
+                AppConfig.gitHubAuthToken.value = password
+                this.password.set("")
+                password
+            }
+        }
+
+        return githubDataSource.login(userName, password).doOnNext {
+            this.userName.set("")
+            this.password.set("")
+        }
     }
 }
